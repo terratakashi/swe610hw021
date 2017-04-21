776b831b6d44cd84fe54b8eab7ab0a422874cbac
Alex Chen <alex@spoonrocket.com>
2017-04-20 23:18:44 -0700

    added the answer for question 2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,11 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  def initialize(name)
+    @name = name
+  end
+
+  def bar(front, raw_hash)
+    rear = raw_hash.keys.sort.first
+    "#{front}#{@name}#{rear}"
+  end
 end
